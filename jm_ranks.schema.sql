03d32f57423e46ec8dd53b30331728c6c2e33bb4
thekot <rereflex@gmail.com>
2015-04-28 01:15:39 +0300

    updated schema

--- jm_ranks.schema.sql
+++ jm_ranks.schema.sql
@@ -15,10 +15,11 @@ CREATE TABLE pointCaps (
   playerId int(10) unsigned NOT NULL,
   playerRole enum('soldier','demoman') NOT NULL,
   pointId int(10) unsigned NOT NULL,
-  capsCount int(10) unsigned NOT NULL,
+  capsCount int(10) unsigned NOT NULL DEFAULT '1',
   lastCap timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (id),
-  UNIQUE KEY idx_pointCaps_unique (playerId,playerRole,pointId)
+  UNIQUE KEY idx_pointCaps_unique (playerId,playerRole,pointId),
+  KEY idx_pointCaps_role_lastCap (playerRole,lastCap)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 CREATE TABLE points (
@@ -60,38 +61,6 @@ BEGIN
 
     INSERT INTO pointCaps (playerId, playerRole, pointId) VALUES (_playerId, 'demoman', _pointId)
     ON DUPLICATE KEY UPDATE capsCount = capsCount + 1, lastCap = CURRENT_TIMESTAMP;
-  END IF;
-END
-;;
-DELIMITER ;
-
--- SAFE TO DELETE --
-DELIMITER ;;
-CREATE PROCEDURE _playerCapturedPointMigrate(_steamId varchar(32), _playerName varchar(64), _playerRole tinyint, _map varchar(64), _areaId tinyint, _areaName varchar(64), _timestamp int)
-BEGIN
-  DECLARE _pointId int;
-  DECLARE _playerId int;
-
-  INSERT INTO players (steamId, name, lastCap) VALUES (_steamId, _playerName, _timestamp)
-  ON DUPLICATE KEY UPDATE id = LAST_INSERT_ID(id), name = _playerName, lastCap = _timestamp;
-  SELECT LAST_INSERT_ID() INTO _playerId;
-
-  IF (_playerRole = 3) THEN
-    INSERT INTO points (map, areaId, areaName, capsSoldier) VALUES (_map, _areaId, _areaName, 1)
-    ON DUPLICATE KEY UPDATE id = LAST_INSERT_ID(id), capsSoldier = capsSoldier + 1;
-    SELECT LAST_INSERT_ID() INTO _pointId;
-
-    INSERT INTO pointCaps (playerId, playerRole, pointId, lastCap) VALUES (_playerId, 'soldier', _pointId, _timestamp)
-    ON DUPLICATE KEY UPDATE capsCount = capsCount + 1, lastCap = _timestamp;
-  END IF;
-
-  IF (_playerRole = 4) THEN
-    INSERT INTO points (map, areaId, areaName, capsDemoman) VALUES (_map, _areaId, _areaName, 1)
-    ON DUPLICATE KEY UPDATE id = LAST_INSERT_ID(id), capsDemoman = capsDemoman + 1;
-    SELECT LAST_INSERT_ID() INTO _pointId;
-
-    INSERT INTO pointCaps (playerId, playerRole, pointId, lastCap) VALUES (_playerId, 'demoman', _pointId, _timestamp)
-    ON DUPLICATE KEY UPDATE capsCount = capsCount + 1, lastCap = _timestamp;
   END IF;
 END
 ;;
